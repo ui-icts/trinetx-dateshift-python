975a58506725d4b19f96cb13fe8255688b8b227c
izzy neuhaus <36773942+izneuhaus@users.noreply.github.com>
2021-04-29 13:43:37 -0500

    Improve performance and add better status/progress/error messages

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from tkinter import Tk
 from tkinter import filedialog
 import os
+from shutil import copyfile
 import pandas as pd
+from tqdm import tqdm
 
 id_column = 'patient_id'
 target_data_type = 'DATETIME (YYYYMMDD)'
@@ -9,19 +11,26 @@ dd_filename = 'datadictionary.xlsx'
 
 
 # adapted from REDCap
-def get_shift_days(hex_id):
-    date_shift_max = 10
-    sub_len = 8
-    dec = int(hex_id[10:][:sub_len], 16)
-    return round(dec / pow(10, sub_len) * date_shift_max)
+def get_shift_days(hex_series):
+    hex_series = hex_series.str.slice(start=10, step=4)
+    hex_series = hex_series.apply(int, base=16)
+    shift_days = round(hex_series / pow(10, 8))
+    return shift_days
 
 
 root = Tk()
 root.withdraw()
 
 dataset_dir = filedialog.askdirectory(title='Select dataset folder')
+shifted_dir = os.path.join(dataset_dir, 'shifted')
 
-print(dataset_dir)
+root.update()
+root.destroy()
+
+try:
+    os.mkdir(shifted_dir)
+except OSError as error:
+    print(error)
 
 datadict_df = pd.read_excel(os.path.join(dataset_dir, dd_filename))
 is_datetime = datadict_df['Data Type'] == target_data_type
@@ -35,15 +44,38 @@ for index, row in datadict_df[is_datetime].iterrows():
 
     date_element_lookup.setdefault(file_name, []).append(element_name)
 
-# for each csv
-for filename in date_element_lookup:
-    try:
-        df = pd.read_csv(os.path.join(dataset_dir, filename))
+# iterate through all csv files
+for entry in os.scandir(dataset_dir):
+    if entry.path.endswith(".csv") and entry.is_file():
+        filename = entry.name
+        source_file = os.path.join(dataset_dir, filename)
+        shifted_file = os.path.join(shifted_dir, filename)
 
-        # shift dates by x days (calculated using id column)
-        for element in date_element_lookup[filename]:
-            df[element] = pd.PeriodIndex(df[element], freq='D')
-            shift_days_lookup = df[id_column].apply(get_shift_days)
-            df[element] -= shift_days_lookup
-    except FileNotFoundError:
-        print("'" + filename + "'" + ' not found, skipping')
+        print("Processing '" + filename + "'...", end="")
+
+        if filename in date_element_lookup.keys():
+            try:
+                df = pd.read_csv(source_file, parse_dates=date_element_lookup[filename])
+
+                if len(df) > 0:
+                    print("found " + str(len(date_element_lookup[filename])) + " date column(s), shifting...")
+
+                    # shift dates by x days (calculated using id column)
+                    for date_element in date_element_lookup[filename]:
+                        df[date_element] = tqdm(
+                            df[date_element] - pd.to_timedelta(get_shift_days(df[id_column]), 'D'),
+                            desc=date_element
+                        )
+
+                    # save shifted csv
+                    print("Saving shifted file to " + shifted_file)
+                    df.to_csv(shifted_file, index=False, header=True, date_format='%Y%m%d')
+                else:
+                    print('zero rows found, skipping')
+
+            except FileNotFoundError:
+                print('file not found, skipping')
+        else:
+            # no dates to shift, just copy file
+            print('no date columns found, copying as-is')
+            copyfile(source_file, shifted_file)
